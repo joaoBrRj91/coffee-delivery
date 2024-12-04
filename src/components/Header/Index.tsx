6a4fcf039c4cd93a4c39855cf7515c7816131652
joaoBrRj91 <joao1991algoritmo@gmail.com>
2024-12-04 19:37:10 -0300

    Header Component - Structure

--- src/components/Header/Index.tsx
+++ src/components/Header/Index.tsx
@@ -1,15 +1,34 @@
-import { HeaderContainer } from "./Styles";
-import  logoCoffeDelivery  from '../../assets/logo.svg'
-import { ShoppingCart, MapPin } from '@phosphor-icons/react'
+import { Link } from "react-router-dom";
+import { ShoppingCart, MapPin } from "@phosphor-icons/react";
+
+import { HeaderContainer, Aside } from "./Styles";
+import logoCoffeDelivery from "../../assets/logo.svg";
+
+interface Product {
+  name: string;
+  value: number;
+}
+
+const cart: Product[] = [{ name: "Latte", value: 120.42 }];
 
 export function Header() {
-    return (
-        <HeaderContainer>
-            <img src={logoCoffeDelivery} alt="" />
-            <nav>
-                <span><ShoppingCart size={22} /></span>
-                <span><MapPin size={22} /></span>
-            </nav>
-        </HeaderContainer>
-    )
+  return (
+    <HeaderContainer>
+      <Link to="/">
+        <img src={logoCoffeDelivery} alt="Coffee Delivery" />
+      </Link>
+
+      <Aside>
+        <div>
+          <MapPin size={22} weight="fill" />
+          <span>Estado, (UF)</span>
+        </div>
+
+        <Link to="/" aria-disabled={cart.length === 0}>
+          <ShoppingCart size={22} />
+        </Link>
+        {cart.length > 0 ? <span>{cart.length}</span> : null}
+      </Aside>
+    </HeaderContainer>
+  );
 }
--- src/components/Header/Styles.tsx
+++ src/components/Header/Styles.tsx
@@ -1,3 +1,5 @@
 import styled from "styled-components";
 
 export const HeaderContainer = styled.header``;
+
+export const Aside = styled.aside``;
